34e33b799fb6682c1bfa64ce3060a8bf43fe1fe1
Durg-PG <85075238+Durg-PG@users.noreply.github.com>
2024-01-29 05:37:38 +0000

    Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+console.log(typeof someNumber)
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+//**************Operations****************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
