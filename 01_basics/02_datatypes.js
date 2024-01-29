148148851d939a4a4e229778a198baf6bd8b37a5
Durg-PG <85075238+Durg-PG@users.noreply.github.com>
2024-01-29 05:19:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "Durgesh"
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+// number => to power 53
+//big Int
+//string =>
+// null => standalone value 
+// undefined => 
+// Symbol (React - "Uniqueness")
+
+
+
+//Object 
+
+console.log(typeof age);
+console.log(typeof null);
